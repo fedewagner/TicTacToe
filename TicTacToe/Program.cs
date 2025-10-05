8c528c45ae1df2d5f7e203fc5716b984b946ab37
fedewagner <federico.j.wagner@gmail.com>
2025-10-05 22:19:20 +0200

    included some comments

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,8 +4,13 @@
     {
         static void Main(string[] args)
         {
+            //Just welcome the user with general information
             UserInterface.WelcomeUser();
+            
+            //explains the positions to the user
             UserInterface.PrintEmptyGrid();
+            
+            //shows how the current grid looks
             UserInterface.PrintCurrentGrid();
         }
     }
