56a840dfcc774a3c911d0884e070dda4b014403f
fedewagner <federico.j.wagner@gmail.com>
2025-10-05 22:05:37 +0200

    Welcome User is implemented

--- TicTacToe/UserInterface.cs
+++ TicTacToe/UserInterface.cs
@@ -2,5 +2,12 @@ namespace TicTacToe;
 
 public class UserInterface
 {
+    public static void WelcomeUser()
+    {
+        Console.WriteLine("Welcome to TicTacToe!");
+        Console.WriteLine("You have to place your Symbol 'O' and play against AI which will be using 'X'");
+        Console.WriteLine("The first who makes 3 in a line, wins!");
+    }
+
     
 }
